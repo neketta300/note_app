c941402a82d476413d7607bda6cf791c4b4e96ef
neketta300 <neketta300@gmail.com>
2025-09-19 18:27:22 +0300

    Внедрил блок в search_screen

--- lib/features/search/view/search_screen.dart
+++ lib/features/search/view/search_screen.dart
@@ -1,96 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:notes_app/bloc/note_bloc/bloc/note_bloc.dart';
 import 'package:notes_app/helpers/responsive_size.dart';
 
 import '../../../widgets/widgets.dart';
+import '../../create/view/create_screen.dart';
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+  SearchScreen({super.key});
+
+  final TextEditingController searchController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xff252525),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 21),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: ResponsiveSize.fromFigmaHeight(context, 88),
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Theme(
-                data: Theme.of(context).copyWith(
-                  textSelectionTheme: const TextSelectionThemeData(
-                    selectionColor: Color(0xff9A9A9A),
-                    selectionHandleColor: Color(0xff9A9A9A),
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<NoteBloc>().add(StartSearchEvent());
+    });
+
+    return BlocBuilder<NoteBloc, NoteState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Color(0xff252525),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 21),
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: ResponsiveSize.fromFigmaHeight(
+                      context,
+                      88,
+                    ),
                   ),
                 ),
-                child: TextField(
-                  cursorColor: Color(0xff9A9A9A),
-                  style: GoogleFonts.nunito(
-                    fontSize: ResponsiveSize.responsiveFontSize(
-                      context,
-                      20,
+                SliverToBoxAdapter(
+                  child: Theme(
+                    data: Theme.of(context).copyWith(
+                      textSelectionTheme:
+                          const TextSelectionThemeData(
+                            selectionColor: Color(0xff9A9A9A),
+                            selectionHandleColor: Color(0xff9A9A9A),
+                          ),
                     ),
-                    fontWeight: FontWeight.w300,
-                    color: Color(0xffCCCCCC),
-                  ),
-                  decoration: InputDecoration(
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pop();
+                    child: TextField(
+                      onChanged: (value) {
+                        context.read<NoteBloc>().add(
+                          SearchNotesEvent(query: value),
+                        );
                       },
-                      child: Icon(
-                        Icons.cancel,
+                      controller: searchController,
+                      cursorColor: Color(0xff9A9A9A),
+                      style: GoogleFonts.nunito(
+                        fontSize: ResponsiveSize.responsiveFontSize(
+                          context,
+                          20,
+                        ),
+                        fontWeight: FontWeight.w300,
                         color: Color(0xffCCCCCC),
                       ),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: ResponsiveSize.responsiveFontSize(
-                        context,
-                        30,
-                      ),
-                    ),
-                    filled: true,
-                    fillColor: Color(0xff3B3B3B),
-                    hintText: 'Search by the keyword...',
-                    hintStyle: GoogleFonts.nunito(
-                      color: Color(0xFFCCCCCC),
-                      fontWeight: FontWeight.w300,
-                      fontSize: ResponsiveSize.responsiveFontSize(
-                        context,
-                        20,
-                      ),
-                    ),
+                      decoration: InputDecoration(
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            context.read<NoteBloc>().add(
+                              LoadNoteEvent(),
+                            );
+                            Navigator.of(context).pop();
+                          },
+                          child: Icon(
+                            Icons.cancel,
+                            color: Color(0xffCCCCCC),
+                          ),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal:
+                              ResponsiveSize.responsiveFontSize(
+                                context,
+                                30,
+                              ),
+                        ),
+                        filled: true,
+                        fillColor: Color(0xff3B3B3B),
+                        hintText: 'Search by the keyword...',
+                        hintStyle: GoogleFonts.nunito(
+                          color: Color(0xFFCCCCCC),
+                          fontWeight: FontWeight.w300,
+                          fontSize: ResponsiveSize.responsiveFontSize(
+                            context,
+                            20,
+                          ),
+                        ),
 
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: ResponsiveSize.fromFigmaHeight(
+                      context,
+                      21,
+                    ),
+                  ),
+                ),
+                if (state is SearchInitialState)
+                  SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Start typing to search...',
+                            style: GoogleFonts.nunito(
+                              fontSize:
+                                  ResponsiveSize.responsiveFontSize(
+                                    context,
+                                    20,
+                                  ),
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                if (state is NoteLoadingState)
+                  SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                if (state is NoteLoadedState)
+                  SliverList.builder(
+                    itemCount: state.notes.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final notes = state.notes;
+                      final note = notes[index];
+                      final title = note.title;
+                      final color = note.colorValue;
+                      return NoteTile(
+                        color: color,
+                        text: title,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) =>
+                                  CreateScreen(note: note),
+                            ),
+                          );
+                        },
+                        onLongPress: () {},
+                      );
+                    },
+                  ),
+              ],
             ),
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: ResponsiveSize.fromFigmaHeight(context, 21),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate((context, index) {
-                return NoteTile(
-                  color: Colors.transparent,
-                  text: '',
-                  onTap: () {},
-                );
-              }, childCount: 1),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
