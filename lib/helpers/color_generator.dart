1aad1545d0144d979a454b68d8f75bbc4a39d887
neketta300 <neketta300@gmail.com>
2025-09-19 18:26:41 +0300

    Изменил генератор цветов

--- lib/helpers/color_generator.dart
+++ lib/helpers/color_generator.dart
@@ -2,12 +2,15 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-Color getRandomColor() {
-  final random = Random();
-  return Color.fromARGB(
-    256,
-    random.nextInt(256),
-    random.nextInt(256),
-    random.nextInt(256),
-  );
+class ColorGenerator {
+  static Color getPastelColor() {
+    final random = Random();
+    final hsl = HSLColor.fromAHSL(
+      1.0, // Альфа
+      random.nextDouble() * 360, // Оттенок: весь спектр
+      0.5 + random.nextDouble() * 0.3, // Насыщенность: 0.4–0.7
+      0.65 + random.nextDouble() * 0.15, // Яркость: 0.85–0.95
+    );
+    return hsl.toColor();
+  }
 }
