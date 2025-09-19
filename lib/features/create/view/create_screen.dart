842d688d9af2313fd1d9ecf30c2f8faeb62ebe21
neketta300 <neketta300@gmail.com>
2025-09-19 18:27:39 +0300

    Внедрил блок в create_screen

--- lib/features/create/view/create_screen.dart
+++ lib/features/create/view/create_screen.dart
@@ -1,149 +1,165 @@
 import 'dart:developer';
 
+import 'package:drift/drift.dart' show Value;
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/database/note/note_database.dart';
+import 'package:notes_app/helpers/color_generator.dart';
 import 'package:notes_app/widgets/app_bar_button.dart';
 
+import '../../../bloc/note_bloc/bloc/note_bloc.dart';
 import '../../../helpers/helpers.dart';
+import '../widgets/widgets.dart';
 
 class CreateScreen extends StatelessWidget {
-  final String? title;
-  final String? text;
-  const CreateScreen({super.key, this.title, this.text});
+  final Note? note;
+  const CreateScreen({super.key, this.note});
 
   @override
   Widget build(BuildContext context) {
     final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
     final TextEditingController titleEditingController =
-        TextEditingController();
+        TextEditingController(text: note?.title ?? '');
     final TextEditingController textEditingController =
-        TextEditingController();
+        TextEditingController(text: note?.body ?? '');
 
-    return Scaffold(
-      backgroundColor: Color(0xff252525),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: ResponsiveSize.fromFigmaWidth(context, 21),
-        ).copyWith(top: ResponsiveSize.fromFigmaHeight(context, 15)),
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.transparent,
-              floating: true,
-              pinned: true,
-              actions: [
-                AppBarButton(
-                  icon: Icons.arrow_back,
-                  onTap: Navigator.of(context).pop,
+    return BlocBuilder<NoteBloc, NoteState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Color(0xff252525),
+          body: Padding(
+            padding:
+                EdgeInsets.symmetric(
+                  horizontal: ResponsiveSize.fromFigmaWidth(
+                    context,
+                    21,
+                  ),
+                ).copyWith(
+                  top: ResponsiveSize.fromFigmaHeight(context, 15),
                 ),
-                Spacer(),
-                AppBarButton(icon: Icons.looks, onTap: null),
-                SizedBox(
-                  width: ResponsiveSize.fromFigmaWidth(context, 21),
-                ),
-                AppBarButton(icon: Icons.save, onTap: () {}),
-              ],
-            ),
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: ResponsiveSize.fromFigmaHeight(context, 40),
-              ),
-            ),
-
-            SliverToBoxAdapter(
-              child: Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    CreateNoteTextField(
-                      hintSize: 48,
-                      textSize: 38,
-                      hintText: 'Title',
-                      textEditingController: titleEditingController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          log('Введите заголов');
-                        }
-                        return null;
-                      },
-                      autofocus: true,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  backgroundColor: Colors.transparent,
+                  floating: true,
+                  pinned: true,
+                  actions: [
+                    AppBarButton(
+                      icon: Icons.arrow_back,
+                      onTap: Navigator.of(context).pop,
                     ),
-
+                    Spacer(),
+                    AppBarButton(icon: Icons.looks, onTap: () {}),
                     SizedBox(
-                      height: ResponsiveSize.fromFigmaHeight(
+                      width: ResponsiveSize.fromFigmaWidth(
                         context,
-                        20,
+                        21,
                       ),
                     ),
-
-                    CreateNoteTextField(
-                      hintSize: 23,
-                      textSize: 23,
-                      hintText: 'Type something...',
-                      textEditingController: textEditingController,
-                      autofocus: false,
+                    AppBarButton(
+                      icon: Icons.save,
+                      onTap: () {
+                        _saveNote(
+                          formKey: formKey,
+                          titleEditingController:
+                              titleEditingController,
+                          textEditingController:
+                              textEditingController,
+                          context: context,
+                          note: note,
+                        );
+                      },
                     ),
                   ],
                 ),
-              ),
+                SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: ResponsiveSize.fromFigmaHeight(
+                      context,
+                      5,
+                    ),
+                  ),
+                ),
+
+                SliverToBoxAdapter(
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        CreateNoteTextField(
+                          hintSize: 48,
+                          textSize: 38,
+                          hintText: 'Title',
+                          textEditingController:
+                              titleEditingController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              log('Введите заголов');
+                            }
+                            return null;
+                          },
+                          autofocus: true,
+                        ),
+
+                        SizedBox(
+                          height: ResponsiveSize.fromFigmaHeight(
+                            context,
+                            20,
+                          ),
+                        ),
+
+                        CreateNoteTextField(
+                          hintSize: 23,
+                          textSize: 23,
+                          hintText: 'Type something...',
+                          textEditingController:
+                              textEditingController,
+                          autofocus: false,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
-}
 
-class CreateNoteTextField extends StatelessWidget {
-  const CreateNoteTextField({
-    super.key,
-    required this.hintSize,
-    required this.textSize,
-    required this.hintText,
-    required this.textEditingController,
-    required this.autofocus,
-    this.validator,
-  });
-  final TextEditingController textEditingController;
-  final double hintSize;
-  final double textSize;
-  final String hintText;
-  final bool autofocus;
-  final String? Function(String?)? validator;
-  @override
-  Widget build(BuildContext context) {
-    return Theme(
-      data: Theme.of(context).copyWith(
-        textSelectionTheme: const TextSelectionThemeData(
-          selectionColor: Color(0xff9A9A9A),
-          selectionHandleColor: Color(0xff9A9A9A),
-        ),
-      ),
-      child: TextFormField(
-        autofocus: autofocus,
-        controller: textEditingController,
-        validator: validator,
-        cursorColor: Color(0xff9A9A9A),
-        style: GoogleFonts.nunito(
-          fontSize: ResponsiveSize.responsiveFontSize(
-            context,
-            textSize,
-          ),
-          color: Colors.white,
-        ),
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: hintText,
-          hintStyle: GoogleFonts.nunito(
-            fontSize: ResponsiveSize.responsiveFontSize(
-              context,
-              hintSize,
-            ),
-            color: Color(0xff9A9A9A),
-          ),
-        ),
-      ),
-    );
+  void _saveNote({
+    required GlobalKey<FormState> formKey,
+    required TextEditingController titleEditingController,
+    required TextEditingController textEditingController,
+    required BuildContext context,
+    required Note? note,
+  }) {
+    if (!formKey.currentState!.validate()) return;
+    final String title = titleEditingController.text.trim();
+    final String body = textEditingController.text.trim();
+
+    if (note != null) {
+      final newNote = Note(
+        id: note.id,
+        title: title,
+        body: body,
+        createAt: note.createAt,
+        colorValue: note.colorValue,
+      );
+      context.read<NoteBloc>().add(UpdateNoteEvent(note: newNote));
+    } else {
+      final color = ColorGenerator.getPastelColor().toARGB32();
+      final newNote = NotesCompanion.insert(
+        title: title,
+        body: body,
+        createAt: DateTime.timestamp(),
+        colorValue: Value(color),
+      );
+      context.read<NoteBloc>().add(AddNoteEvent(note: newNote));
+    }
+
+    Navigator.of(context).pop();
   }
 }
