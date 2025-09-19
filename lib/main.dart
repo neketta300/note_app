df5eb30d53984c8db2f12ca3d93bea5a9cf762da
neketta300 <neketta300@gmail.com>
2025-09-19 18:28:50 +0300

    Внедрил конфиг в main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:notes_app/app/app_config.dart';
 import 'package:notes_app/app/note_app.dart';
+import 'package:notes_app/database/note/note_database.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const NoteApp());
+  try {
+    final db = NoteDatabase();
+    final appConfig = _initAppConfig(db);
+    runApp(NoteApp(appConfig: appConfig));
+  } catch (e) {
+    log('Ошибка инициализации конфига');
+  }
 }
+
+AppConfig _initAppConfig(NoteDatabase noteDatabase) =>
+    AppConfig(noteDatabase: noteDatabase);
