ae7ef546dba395f52482d897a147e19c995bffa5
neketta300 <neketta300@gmail.com>
2025-09-23 16:01:42 +0300

    Добавил диалоговое окно при попытке выхода не сохранившис

--- lib/features/create/view/create_screen.dart
+++ lib/features/create/view/create_screen.dart
@@ -3,130 +3,246 @@ import 'dart:developer';
 import 'package:drift/drift.dart' show Value;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notes_app/database/note/note_database.dart';
-import 'package:notes_app/helpers/color_generator.dart';
-import 'package:notes_app/widgets/app_bar_button.dart';
 
 import '../../../bloc/note_bloc/bloc/note_bloc.dart';
 import '../../../helpers/helpers.dart';
+import '../../../widgets/widgets.dart';
 import '../widgets/widgets.dart';
 
-class CreateScreen extends StatelessWidget {
+class CreateScreen extends StatefulWidget {
+  final bool editStatus;
   final Note? note;
-  const CreateScreen({super.key, this.note});
+  const CreateScreen({
+    super.key,
+    this.note,
+    required this.editStatus,
+  });
+
+  @override
+  State<CreateScreen> createState() => _CreateScreenState();
+}
+
+class _CreateScreenState extends State<CreateScreen> {
+  late bool currentEditStatus;
+  late String initialTitle;
+  late String initialBody;
+
+  late TextEditingController titleEditingController;
+  late TextEditingController textEditingController;
+  @override
+  void initState() {
+    super.initState();
+    currentEditStatus = widget.editStatus;
+
+    initialTitle = widget.note?.title ?? '';
+    initialBody = widget.note?.body ?? '';
+
+    titleEditingController = TextEditingController(
+      text: initialTitle,
+    );
+    textEditingController = TextEditingController(text: initialBody);
+  }
 
   @override
   Widget build(BuildContext context) {
     final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-    final TextEditingController titleEditingController =
-        TextEditingController(text: note?.title ?? '');
-    final TextEditingController textEditingController =
-        TextEditingController(text: note?.body ?? '');
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, results) async {
+        if (didPop) return;
+        final hasChanges =
+            titleEditingController.text.trim() !=
+                initialTitle.trim() ||
+            textEditingController.text.trim() != initialBody.trim();
 
-    return BlocBuilder<NoteBloc, NoteState>(
-      builder: (context, state) {
-        return Scaffold(
-          backgroundColor: Color(0xff252525),
-          body: Padding(
-            padding:
-                EdgeInsets.symmetric(
-                  horizontal: ResponsiveSize.fromFigmaWidth(
-                    context,
-                    21,
-                  ),
-                ).copyWith(
-                  top: ResponsiveSize.fromFigmaHeight(context, 15),
-                ),
-            child: CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  backgroundColor: Colors.transparent,
-                  floating: true,
-                  pinned: true,
-                  actions: [
-                    AppBarButton(
-                      icon: Icons.arrow_back,
-                      onTap: Navigator.of(context).pop,
+        if (hasChanges) {
+          final shouldExit = await _showExitDialog(context);
+          if (shouldExit) Navigator.of(context).pop();
+        } else {
+          Navigator.of(context).pop();
+        }
+      },
+      child: BlocBuilder<NoteBloc, NoteState>(
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: Color(0xff252525),
+            body: Padding(
+              padding:
+                  EdgeInsets.symmetric(
+                    horizontal: ResponsiveSize.fromFigmaWidth(
+                      context,
+                      21,
                     ),
-                    Spacer(),
-                    AppBarButton(icon: Icons.looks, onTap: () {}),
-                    SizedBox(
-                      width: ResponsiveSize.fromFigmaWidth(
+                  ).copyWith(
+                    top: ResponsiveSize.fromFigmaHeight(context, 15),
+                  ),
+              child: CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    backgroundColor: Colors.transparent,
+                    floating: true,
+                    pinned: true,
+                    actions: [
+                      AppBarButton(
+                        icon: Icons.arrow_back,
+                        onTap: () {
+                          Navigator.maybePop(context);
+                        },
+                      ),
+                      Spacer(),
+                      currentEditStatus
+                          ? Row(
+                              children: [
+                                AppBarButton(
+                                  icon: Icons.looks,
+                                  onTap: () {
+                                    currentEditStatus =
+                                        !currentEditStatus;
+                                    setState(() {});
+                                  },
+                                ),
+                                SizedBox(
+                                  width:
+                                      ResponsiveSize.fromFigmaWidth(
+                                        context,
+                                        21,
+                                      ),
+                                ),
+                                AppBarButton(
+                                  icon: Icons.save,
+                                  onTap: () {
+                                    _saveNote(
+                                      formKey: formKey,
+                                      titleEditingController:
+                                          titleEditingController,
+                                      textEditingController:
+                                          textEditingController,
+                                      context: context,
+                                      note: widget.note,
+                                    );
+                                  },
+                                ),
+                              ],
+                            )
+                          : AppBarButton(
+                              icon: Icons.edit,
+                              onTap: () {
+                                setState(() {
+                                  currentEditStatus =
+                                      !currentEditStatus;
+                                });
+                              },
+                            ),
+                    ],
+                  ),
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: ResponsiveSize.fromFigmaHeight(
                         context,
-                        21,
+                        5,
                       ),
                     ),
-                    AppBarButton(
-                      icon: Icons.save,
-                      onTap: () {
-                        _saveNote(
-                          formKey: formKey,
-                          titleEditingController:
-                              titleEditingController,
-                          textEditingController:
-                              textEditingController,
-                          context: context,
-                          note: note,
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                SliverToBoxAdapter(
-                  child: SizedBox(
-                    height: ResponsiveSize.fromFigmaHeight(
-                      context,
-                      5,
+                  ),
+
+                  SliverToBoxAdapter(
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          CreateNoteTextField(
+                            hintSize: 48,
+                            textSize: 38,
+                            hintText: 'Title',
+                            textEditingController:
+                                titleEditingController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                log('Введите заголов');
+                              }
+                              return null;
+                            },
+                            autofocus: true,
+                            enabled: currentEditStatus,
+                          ),
+
+                          SizedBox(
+                            height: ResponsiveSize.fromFigmaHeight(
+                              context,
+                              20,
+                            ),
+                          ),
+
+                          CreateNoteTextField(
+                            hintSize: 23,
+                            textSize: 23,
+                            hintText: 'Type something...',
+                            textEditingController:
+                                textEditingController,
+                            autofocus: false,
+                            enabled: currentEditStatus,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
 
-                SliverToBoxAdapter(
-                  child: Form(
-                    key: formKey,
-                    child: Column(
-                      children: [
-                        CreateNoteTextField(
-                          hintSize: 48,
-                          textSize: 38,
-                          hintText: 'Title',
-                          textEditingController:
-                              titleEditingController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              log('Введите заголов');
-                            }
-                            return null;
-                          },
-                          autofocus: true,
-                        ),
-
-                        SizedBox(
-                          height: ResponsiveSize.fromFigmaHeight(
-                            context,
-                            20,
-                          ),
-                        ),
-
-                        CreateNoteTextField(
-                          hintSize: 23,
-                          textSize: 23,
-                          hintText: 'Type something...',
-                          textEditingController:
-                              textEditingController,
-                          autofocus: false,
-                        ),
-                      ],
-                    ),
+  Future<bool> _showExitDialog(BuildContext context) async {
+    return await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            backgroundColor: Color(0xff252525),
+            title: Icon(
+              Icons.info,
+              color: Color(0xff606060),
+              size: 32,
+            ),
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Выйти без сохранения?',
+                  maxLines: 1,
+                  style: GoogleFonts.nunito(
+                    color: Color(0xffCFCFCF),
+                    fontSize: 23,
                   ),
                 ),
               ],
             ),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AlertButton(
+                    color: Colors.red,
+                    text: 'Выйти',
+                    onTap: () => Navigator.of(context).pop(true),
+                  ),
+                  SizedBox(
+                    width: ResponsiveSize.fromFigmaWidth(context, 35),
+                  ),
+                  AlertButton(
+                    color: Colors.green,
+                    text: 'Остаться',
+                    onTap: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+            ],
           ),
-        );
-      },
-    );
+        ) ??
+        false;
   }
 
   void _saveNote({
@@ -149,6 +265,8 @@ class CreateScreen extends StatelessWidget {
         colorValue: note.colorValue,
       );
       context.read<NoteBloc>().add(UpdateNoteEvent(note: newNote));
+      initialBody = body;
+      initialTitle = title;
     } else {
       final color = ColorGenerator.getPastelColor().toARGB32();
       final newNote = NotesCompanion.insert(
@@ -158,8 +276,8 @@ class CreateScreen extends StatelessWidget {
         colorValue: Value(color),
       );
       context.read<NoteBloc>().add(AddNoteEvent(note: newNote));
+      initialBody = body;
+      initialTitle = title;
     }
-
-    Navigator.of(context).pop();
   }
 }
